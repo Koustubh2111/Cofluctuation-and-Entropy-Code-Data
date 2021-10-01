8936a3e14a2898dfda5affc2e439852eae33d3ba
Koustubh2111 <36010817+Koustubh2111@users.noreply.github.com>
2021-10-01 02:34:49 -0300

    Delete Entropy Baseline directory

--- Entropy Baseline/entropy_fitlme.m
+++
@@ -1,8 +0,0 @@
-
-%% ANIMAL TYPE (hf/normal) IS FIXED EFFECT, CHANNEL AND ANIMAL ID IS RANDOM EFFECT
-T = readtable('entropy_all_numerical2.csv');
-
-lme=fitlme(T, 'entropy_mean ~ animal_type + (1|channel) + (1|animal)')
-lme=fitlme(T, 'entropy_std ~ animal_type + (1|channel) + (1|animal)')
-
-
